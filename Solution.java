d632d84f9fc5d34941a410af0810aa382367a9fc
sdim-ua <ingersol@yandex.ua>
2019-01-08 14:48:41 +0300

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -8,9 +8,9 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String name = reader.readLine();
         String sAge = reader.readLine();
-        int nAge = Integer.parseInt(sAge);
+        int nAge = Integer.parseInt(sAge); 
+        String sAge = reader.readLine(); 
         System.out.println(name + " захватит мир через " + sAge + " лет. Му-ха-ха!");
         
     }
